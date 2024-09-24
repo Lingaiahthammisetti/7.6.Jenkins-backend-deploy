9edfe2159f9566347feb90e2b494ae645017a553
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-09-24 23:33:04 +0530

    Jenkins backend deploy msg

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -8,13 +8,14 @@ variable "common_tags" {
   default =  {
      Project = "expense"
      Environment = "dev"
-     Terraform = "true"
+     Terraform = true
      Component ="backend"
   }
 }
 variable "zone_name" {
   default = "lingaiah.online"
   }
-  variable "app_version" {
-     
-  }
+
+variable "app_version" {
+
+}
