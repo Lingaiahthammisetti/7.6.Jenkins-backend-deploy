a3911a7725b11abff7f5552a8b0da8988f11f991
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-01-05 10:39:29 +0530

    jenkins backend deploy

--- terraform/backend.sh
+++ terraform/backend.sh
@@ -4,4 +4,4 @@ environment=$2
 app_Version = $3
 dnf install ansible -y
 pip3.9 install botocore boto3
-ansible-pull -i localhost, -U https://github.com/linga-daws-78s/expense-ansible-roles-tf.git main.yaml -e component=$compoent -e env=$environment -e appVersion=$app_Version
+ansible-pull -i localhost, -U https://github.com/Lingaiahthammisetti/4.14.2.roboshop-ansible-roles-tf-Infra-dev.git main-tf.yaml -e component=$compoent -e env=$environment -e appVersion=$app_Version
