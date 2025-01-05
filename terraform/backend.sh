d9a69394ffa8a0f6aa6d179ef8dd45595b103014
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-01-05 11:05:49 +0530

    jenkins backend deploy

--- terraform/backend.sh
+++ terraform/backend.sh
@@ -2,6 +2,7 @@
 component=$1
 environment=$2
 app_Version = $3
+
 dnf install ansible -y
 pip3.9 install botocore boto3
 ansible-pull -i localhost, -U https://github.com/Lingaiahthammisetti/4.10.2.expense-ansible-roles-tf-Infra-Dev.git main-tf.yaml -e component=$component -e env=$environment -e appVersion=$app_Version
